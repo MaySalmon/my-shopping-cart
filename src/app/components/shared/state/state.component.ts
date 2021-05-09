42a0443a713e75fda0e7f6acd717cbc1add791de
MaySalmon <58439353+MaySalmon@users.noreply.github.com>
2021-05-09 14:00:15 +0300

    added functions to State page

--- src/app/components/shared/state/state.component.ts
+++ src/app/components/shared/state/state.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { productsUrl } from 'src/app/config/api';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service'
 
@@ -10,7 +11,11 @@ import { ProductService } from 'src/app/services/product.service'
 export class StateComponent implements OnInit {
 
   productList: Product[] = []
-
+  allProductList: Product[] = []
+  maxProduct: Product[] = []
+  maxnum=0;
+  productname:string;
+  maxindex=0;
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
@@ -20,7 +25,31 @@ export class StateComponent implements OnInit {
   loadProducts(){
     // this.productList = this.productService.getProducts();
       this.productService.getProducts().subscribe((products) =>{
-      this.productList = products;
+      this.allProductList = products;
+      console.log(this.allProductList);
+      for(let k=0; k<5;k++){
+        this.maxnum=0;
+        this.maxindex=0;
+        this.productname="";
+        for(let i=0; i<this.allProductList.length; i++){
+          if(this.allProductList[i].price>this.maxnum)
+          {
+            this.maxnum=this.allProductList[i].price;
+            this.productname=this.allProductList[i].name;
+            this.maxindex=i;
+          }
+        }
+        for(let j=0; j<this.allProductList.length; j++)
+        {
+          if(j == this.maxindex)
+          {
+            this.productList.push(this.allProductList[j]);
+            this.allProductList.splice(j , 1);
+            break;
+          }
+        }
+      }
+      this.maxProduct.push(this.productList[0]);
       
       })
     }
